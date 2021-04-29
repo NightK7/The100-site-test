0f8531285d32249e3422b2606174afa01de526fc
NightK7 <83420732+NightK7@users.noreply.github.com>
2021-04-30 00:56:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// test
+//JS POUR LA PARITES LIEUX
+function myLocation() {
+    var x = document.getElementById("camp100");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+
+function myImageLocation(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+}
