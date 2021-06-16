d8a5c4cad86dcc9975f7837901f1d0ba6cecac16
NightK7 <83420732+NightK7@users.noreply.github.com>
2021-06-16 18:46:15 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,14 +8,16 @@ function goDown() {
 function myLocation(id) {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    var x = document.getElementById(id);
 
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var x = document.getElementById(id);
+    var divs = ['camp100', 'arkadia', 'weather', 'tondc', 'deadzone', 'becca', 'polis', 'shallow', 'polaris', 'ark', 'eligius', 'sanctum', 'nakara', 'etherea', 'skyring', 'bardo'];
+
+    for (var i = 0; i < divs.length; i++) {
+        if (x != document.getElementById(divs[i])) {
+            document.getElementById(divs[i]).style.display = 'none';
+        }
     }
-    else {
-        x.style.display = "none";
-    }
+    x.style.display = 'block';
 }
 
 //IMAGES ZOOM DES LIEUX
